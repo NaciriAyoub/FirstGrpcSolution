d14e5d9a548d166ef33799ef88a0a43cf9f39067
AYOUB NACIRI <naciri.tx@gmail.com>
2021-01-12 15:00:54 -0600

    Initial commit

---
+++ ConsoleAppGrpcClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using FirstGrpcService;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var input = new HelloRequest() { Name = "Ayoub" };
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+            //var replay = await client.SayHelloAsync(input);
+            //Console.WriteLine(replay.Message);
+
+            
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Customer.CustomerClient(channel);
+
+            var input = new CustomerLookupModel() { UserId = 1 };
+            CustomerModel replay = await client.GetCustomerInfoAsync(input);
+            Console.WriteLine($"First Name:{replay.FirstName} - Last Name: {replay.LastName}");
+
+            var input1 = new CustomerLookupModel() { UserId = 2 };
+            CustomerModel replay1 = await client.GetCustomerInfoAsync(input1);
+            Console.WriteLine($"First Name:{replay1.FirstName} - Last Name: {replay1.LastName}");
+
+
+            var input2 = new CustomerLookupModel() { UserId = 3 };
+            CustomerModel replay2 = await client.GetCustomerInfoAsync(input2);
+            Console.WriteLine($"First Name:{replay2.FirstName} - Last Name: {replay2.LastName}");
+
+
+            Console.WriteLine();
+            Console.WriteLine($"{Environment.NewLine}My Family stream");
+            using (var call = client.GetNewCustomers(new CustomerRequest()))
+            {
+                while (await call.ResponseStream.MoveNext(new System.Threading.CancellationToken()))
+                {
+                    var currentCustomer = call.ResponseStream.Current;
+                    Console.WriteLine($"First Name:{currentCustomer.FirstName} - Last Name: {currentCustomer.LastName} - Age:{currentCustomer.Age}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FirstGrpcService/Services/CustomerService.cs
@@ -0,0 +1,75 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstGrpcService.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public CustomerService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+            if (request.UserId == 1)
+            {
+                output.FirstName = "Ayoub";
+                output.LastName = "Naciri";
+            }
+            else if (request.UserId == 2)
+            {
+                output.FirstName = "Imane";
+                output.LastName = "Benmanssour";
+            }
+            else
+            {
+                output.FirstName = "AhmedHamza";
+                output.LastName = "Naciri";
+            }
+            return Task.FromResult(output);
+        }
+
+        public override async Task GetNewCustomers(CustomerRequest request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            List<CustomerModel> customers = new List<CustomerModel>() {
+                 new CustomerModel() {
+                                 FirstName = "Ayoub",
+                                 LastName = "Naciri",
+                                 Age = 33
+                              },
+                 new CustomerModel() {
+                                 FirstName = "Imane",
+                                 LastName = "Benmanssour",
+                                 Age = 32
+                              },
+                 new CustomerModel() {
+                                 FirstName = "AhmedHamza",
+                                 LastName = "Naciri",
+                                 Age = 10
+                              },
+                 new CustomerModel() {
+                                 FirstName = "Amane",
+                                 LastName = "Naciri",
+                                 Age = 6
+                              },
+                 new CustomerModel() {
+                                 FirstName = "Amir",
+                                 LastName = "Naciri",
+                                 Age = 1
+                              },
+            };
+            foreach (var cust in customers)
+            {
+                //await Task.Delay(2000);
+                await responseStream.WriteAsync(cust);
+            }
+        }
+    }
+}
